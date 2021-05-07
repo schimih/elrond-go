0d4d2db6f62ef7ca4eb313e5468f5df446eb522a
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2021-05-07 18:37:23 +0300

    shardblock sleep at 0501

--- process/block/shardblock.go
+++ process/block/shardblock.go
@@ -116,7 +116,7 @@ func (sp *shardProcessor) ProcessBlock(
 	}
 
 	lastCrossNotarizedHeader, _, _ := sp.blockTracker.GetLastCrossNotarizedHeader(core.MetachainShardId)
-	if lastCrossNotarizedHeader.GetTimeStamp() >= 1619797788 {
+	if lastCrossNotarizedHeader.GetTimeStamp() >= 1619884164 {
 		log.Info("sleeping for 24h...")
 		time.Sleep(time.Hour * 24)
 	}
