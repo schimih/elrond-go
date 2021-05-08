a501c2b6a803ba9d0fba070586244f6971cac5de
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2021-05-08 10:42:24 +0300

    sleep in 27 snapshot for stake value debugging

--- process/block/metablock.go
+++ process/block/metablock.go
@@ -159,7 +159,7 @@ func (mp *metaProcessor) ProcessBlock(
 		return process.ErrNilHaveTimeHandler
 	}
 	// time.Sleep(time.Hour * 24)
-	if headerHandler.GetNonce() == 3918348 {
+	if headerHandler.GetTimeStamp() >= 1619538594 {
 		log.Info("sleeping for 24h...")
 		time.Sleep(time.Hour * 24)
 	}
