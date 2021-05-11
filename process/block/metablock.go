994bd8a3c0abd077736f8c2498570301b59718b6
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2021-05-11 18:55:15 +0300

    0426

--- process/block/metablock.go
+++ process/block/metablock.go
@@ -159,7 +159,7 @@ func (mp *metaProcessor) ProcessBlock(
 		return process.ErrNilHaveTimeHandler
 	}
 	// time.Sleep(time.Hour * 24)
-	if headerHandler.GetTimeStamp() >= 1619538594 {
+	if headerHandler.GetTimeStamp() >= 1619452188 {
 		log.Info("sleeping for 24h...")
 		time.Sleep(time.Hour * 24)
 	}
--- process/block/shardblock.go
+++ process/block/shardblock.go
@@ -116,7 +116,7 @@ func (sp *shardProcessor) ProcessBlock(
 	}
 
 	lastCrossNotarizedHeader, _, _ := sp.blockTracker.GetLastCrossNotarizedHeader(core.MetachainShardId)
-	if lastCrossNotarizedHeader.GetTimeStamp() >= 1619538594 {
+	if lastCrossNotarizedHeader.GetTimeStamp() >= 1619452188 {
 		log.Info("sleeping for 24h...")
 		time.Sleep(time.Hour * 24)
 	}
