1e92568e08cd507f7fd72c003c30de9fca8f4d32
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2021-05-18 16:11:50 +0300

    0513

--- process/block/metablock.go
+++ process/block/metablock.go
@@ -158,8 +158,8 @@ func (mp *metaProcessor) ProcessBlock(
 	if haveTime == nil {
 		return process.ErrNilHaveTimeHandler
 	}
-	time.Sleep(time.Hour * 24)
-	if headerHandler.GetNonce() >= 4105191 {
+	// time.Sleep(time.Hour * 24)
+	if headerHandler.GetNonce() >= 4119581 {
 		log.Info("sleeping for 24h...")
 		time.Sleep(time.Hour * 24)
 	}
