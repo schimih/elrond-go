dc67763f8fff5f9dddf9541486f39e77657a624a
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2021-06-22 12:39:20 +0300

    0614

--- process/block/shardblock.go
+++ process/block/shardblock.go
@@ -116,7 +116,7 @@ func (sp *shardProcessor) ProcessBlock(
 	}
 
 	lastCrossNotarizedHeader, _, _ := sp.blockTracker.GetLastCrossNotarizedHeader(core.MetachainShardId)
-	if lastCrossNotarizedHeader.GetNonce() >= 4565317 {
+	if lastCrossNotarizedHeader.GetNonce() >= 4579698 {
 		log.Info("sleeping for 24h...")
 		time.Sleep(time.Hour * 24)
 	}
