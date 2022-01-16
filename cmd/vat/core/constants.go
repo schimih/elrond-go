81f881ea129c3e1164461ebd08c59d56f3774284
schimih <schiopumihaialexandru@gmail.com>
2022-01-16 13:38:19 +0200

    add constants

--- cmd/vat/core/constants.go
+++ cmd/vat/core/constants.go
@@ -1,1 +1,12 @@
 package core
+
+// PortStatus represents a port's status.
+type PortStatus string
+
+// Enumerates the different possible state values.
+const (
+	Open       PortStatus = "open"
+	Closed     PortStatus = "closed"
+	Filtered   PortStatus = "filtered"
+	Unfiltered PortStatus = "unfiltered"
+)
